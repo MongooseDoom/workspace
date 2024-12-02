7dc78154279d895b050d6780a044e734c6cd788b
Jackie Keiser <jackie.keiser@wdgie.com>
2024-12-02 09:31:25 -0600

    Adding files from last laptop refresh

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# preferences
+# Workspace
 My preferences and zsh theme
+
+## Applications
+- [ ] [Firefox](https://www.mozilla.org/en-US/firefox/new/)
+- [ ] [Slack](https://slack.com/downloads/mac)
+- [ ] [VSCode](https://code.visualstudio.com/download)
+- [ ] [Wacom Driver PTH-450](https://www.wacom.com/en-us/support/product-support/drivers)
+- [ ] [iTerm](https://iterm2.com/)
+- [ ] [Discord](https://discord.com/download)
+- [ ] [Filezilla](https://filezilla-project.org/)
+- [ ] [Polypane](https://polypane.app/)
+- [ ] [Homebrew](https://brew.sh/)
+- [ ] [Docker](https://www.docker.com/)
+- [ ] [Sitespeed](https://www.sitespeed.io/)
---
+++ dotfiles/oh-my-zsh/custom/my_aliases.zsh
@@ -0,0 +1,35 @@
+# Add yourself some shortcuts to projects you often work on
+# Example:
+#
+# brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
+#
+
+# -------------------------------------------------------------------
+# Tools
+# -------------------------------------------------------------------
+
+
+# -------------------------------------------------------------------
+# Edit Files
+# -------------------------------------------------------------------
+alias edit_hosts='code /etc/hosts'
+alias edit_aliases="code ~/.oh-my-zsh/custom/my_aliases.zsh"
+# for path
+alias edit_php='code /opt/homebrew/etc/php/8.3/php.ini'
+alias edit_path='code ~/.oh-my-zsh/custom/my_path.zsh'
+alias edit_zsh='code ~/.zshrc'
+
+# -------------------------------------------------------------------
+# Open
+# -------------------------------------------------------------------
+alias open_errors='code ~/.config/valet/Log/php-fpm.log'
+
+# -------------------------------------------------------------------
+# Directories
+# -------------------------------------------------------------------
+
+# -------------------------------------------------------------------
+# Extra
+# -------------------------------------------------------------------
+# To clear terminal
+alias c='clear'
